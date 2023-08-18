1b23fc884395cec4a3e983e96a74257227f7b1dc
ClickadysModding <136777567+ClickadysModding@users.noreply.github.com>
2023-08-18 15:43:30 -0700

    Add files via upload

---
+++ PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace GorillaTagModTemplateProject
+{
+	/// <summary>
+	/// This class is used to provide information about your mod to BepInEx.
+	/// </summary>
+	class PluginInfo
+	{
+		public const string GUID = "com.Soda.gorillatag.PlayerESPModdeds";
+		public const string Name = "PlayerESPModdeds";
+		public const string Version = "1.0.0";
+	}
+}
